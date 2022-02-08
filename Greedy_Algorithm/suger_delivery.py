8324292c979e5b38272313a70295fb4262f01f64
Chang <ckdgusss@gmail.com>
2022-02-08 20:37:15 +0900

    2022/02/08 coding note

---
+++ Greedy_Algorithm/suger_delivery.py
@@ -0,0 +1,11 @@
+n = int(input())
+count = -1
+five_num = n//5
+if n % 5 == 0:
+    count = five_num
+else:
+    for i in reversed(range(five_num+1)):
+        if (n-(i*5)) % 3 == 0:
+            count = int(i + ((n-(i*5))/3))
+            break
+print(count)
