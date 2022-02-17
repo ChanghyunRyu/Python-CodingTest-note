c7014097cda19a62b841ece8c813baa4406adf49
Chang <ckdgusss@gmail.com>
2022-02-17 18:59:33 +0900

    2022/02/17 coding note

--- Sorting/Sort_basic.py
+++ Sorting/Sort_basic.py
@@ -23,3 +23,46 @@ for i in range(1, len(data)):
 print(data)
 
 # 퀵정렬
+data = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+
+def quick_sort(data, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start +1
+    right = end
+    while left <= right:
+        while left <= end and data[left] <= data[pivot]:
+            left += 1
+        while right > start and data[right] >= data[pivot]:
+            right -= 1
+        if left > right:
+            data[right], data[pivot] = data[pivot], data[right]
+        else:
+            data[left], data[right] = data[right], data[left]
+    quick_sort(data, start, right-1)
+    quick_sort(data, right+1, end)
+
+
+quick_sort(data, 0, len(data)-1)
+print(data)
+data = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+
+# 새로 배열을 만들어서 해결하는 방법
+# 평균 시간복잡도 O(n*log(n))
+def quick_sort_python(data):
+    if len(data) <= 1:
+        return data
+
+    pivot = data[0]
+    tail = data[1:]
+
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort_python(left_side) + [pivot] + quick_sort_python(right_side)
+
+
+print(quick_sort_python(data))
