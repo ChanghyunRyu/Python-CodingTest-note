4cb7f077df7e5622aab12cab6cd837ebe7937773
Chang <ckdgusss@gmail.com>
2022-04-14 21:02:15 +0900

    2022/04/14 coding test note-3

---
+++ Backtracking/15651_NandM_3.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+
+
+def NandM(number: int, num_list: list):
+    num_list.append(number)
+    if len(num_list) == m:
+        for num in num_list:
+            print(num, end=' ')
+        print()
+        return
+    else:
+        for i in range(n):
+            NandM(i+1, list(num_list))
+
+
+for i in range(n):
+    NandM(i+1, [])
