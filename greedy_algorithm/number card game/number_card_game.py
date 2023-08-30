f17bbe578cb2d487bb7d15decc1c33316b61e371
chang <ckdgusss@gmail.com>
2023-08-30 16:09:46 +0900

    2023/08/30 greedy algorithm - number card game explanation

---
+++ greedy_algorithm/number card game/number_card_game.py
@@ -0,0 +1,8 @@
+import sys
+n, m = map(int, input().split())
+result = 0
+for _ in range(n):
+    data = list(map(int, sys.stdin.readline().split()))
+    min_value = min(data)
+    result = max(result, min_value)
+print(result)
