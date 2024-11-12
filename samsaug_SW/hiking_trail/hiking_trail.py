4aee24cdd3539f8cbf0c4f91bd06c8672bb4665a
CreGecko <ckdgusss@gmail.com>
2024-11-12 14:46:04 +0900

    SWEA 모의 역량평가 - 등산로 조성 수정

--- samsaug_SW/hiking_trail/hiking_trail.py
+++ samsaug_SW/hiking_trail/hiking_trail.py
@@ -6,21 +6,24 @@ def dfs(result, x, y, k, is_used, route):
     flag = False
     route.append((x, y))
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+        nx = x+dx[i]
+        ny = y+dy[i]
         check = 0 <= nx < n and 0 <= ny < n and (nx, ny) not in route
         if check and arr[x][y] > arr[nx][ny]:
             flag = True
             dfs(result, nx, ny, k, is_used, list(route))
-        elif check and not is_used and arr[x][y] > arr[nx][ny]-k:
-            arr[nx][ny] -= k
-            dfs(result, nx, ny, k, True, list(route))
-            arr[nx][ny] += k
+        if check and not is_used:
+            for j in range(1, k+1):
+                if arr[x][y] > (arr[nx][ny]-j):
+                    arr[nx][ny] -= j
+                    dfs(result, nx, ny, k, True, list(route))
+                    arr[nx][ny] += j
     if not flag:
         result.append(len(route))
 
 
 T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     n, k = map(int, input().split())
     arr = []
