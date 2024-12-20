343fc399d12391129796e0e54e012b3208a55bdb
CreGecko <ckdgusss@gmail.com>
2024-12-20 15:06:14 +0900

    동적 계획법 - 1520번 내리막길 복습

---
+++ dynamic_programing/1520_downhill/downhill_review.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+dp = [[-1]*m for _ in range(n)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+
+def dfs(x, y):
+    if x == n-1 and y == m-1:
+        return 1
+
+    if dp[x][y] != -1:
+        return dp[x][y]
+
+    dp[x][y] = 0
+    for i in range(4):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        check = 0 <= nx < n and 0 <= ny < m
+        if check and arr[x][y] > arr[nx][ny]:
+            dp[x][y] += dfs(nx, ny)
+
+    return dp[x][y]
+
+
+print(dfs(0, 0))
